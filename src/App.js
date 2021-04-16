495280f5b72f1a8f1318239e28854d3bffc1d500
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-04-16 21:52:46 +0300

    render first Item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './App.css';
+import First from './First';
 
-export default function App() {
-  return (
-    <div>
-      Some data:
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        { id: 1, text: 'Animals' },
+        { id: 2, text: 'Anime' },
+        { id: 3, text: 'Anti-Malware' },
+        { id: 4, text: 'Art Design' },
+        { id: 5, text: 'Books' },
+        { id: 6, text: 'Business' },
+        { id: 7, text: 'Calendar' },
+        { id: 8, text: 'Cloud Storage' },
+        { id: 9, text: 'File Sharing' },
+        { id: 10, text: 'Animals' },
+        { id: 11, text: 'Continuous Integration' },
+        { id: 12, text: 'Cryptocurrency' }
+      ]
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        Some data:
+        <ul className="no-bullets">
+          <First key={this.state.list[0].id} text={this.state.list[0].text.toLowerCase()}></First>
+        </ul>
+      </div>
+    );
+  }
 }
+export default App;
